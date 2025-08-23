c983724486bd2c5052ca72944ba114bab23f0c4d
Chase Zhou <chasezhou@MacBook.local>
2025-08-23 11:13:40 -0700

    fix picker highlighting

--- nvim/after/plugin/theme.lua
+++ nvim/after/plugin/theme.lua
@@ -8,5 +8,15 @@ require("monokai-pro").setup({
 			context_start_underline = false,
 		},
 	},
+	override = function(c)
+		local hp = require("monokai-pro.color_helper")
+		local common_fg = hp.lighten(c.sideBar.foreground, 30)
+		return {
+			SnacksPicker = { bg = c.editor.background, fg = common_fg },
+			SnacksPickerBorder = { bg = c.editor.background, fg = c.tab.unfocusedActiveBorder },
+			SnacksPickerTree = { fg = c.editorLineNumber.foreground },
+			NonText = { fg = c.base.dimmed3 }, -- not sure if this should be broken into all hl groups importing NonText
+		}
+	end,
 })
 vim.cmd([[colorscheme monokai-pro]])
